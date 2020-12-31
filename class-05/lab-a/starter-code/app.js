9f16b036efad90c570960f61001edcd7e6693952
semo4 <fotboy1788@hotmail.com>
2020-12-31 13:48:25 +0200

    add sum and multiply function

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -39,7 +39,7 @@ function multiply(a, b) { //eslint-disable-line
         "The product of 5 and 9 is 45."
     ];
 
-    return multiplyNumber;
+    return resultArray;
 
 }
 
@@ -61,11 +61,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+    var sumResult = sum(a,sum(b,c)[0])[0];
+    var multiResult = multiply(a,multiply(b,c)[0])[0];
+
+    var resultArray = [
+        sumResult,
+        multiResult,
+        "4 and 7 and 5 sum to 16.",
+        "The product of 4 and 7 and 5 is 140."
+    ];
+
+    return resultArray;
+
+
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
